387bbfe4e965faa65c66568ea2b39df0e0f0d493
Tiberius Finney <finneyaf16@gmail.com>
2025-12-29 01:53:06 -0500

    added some notes to explain the code I am learning

--- my_first_weather_app/src/main/java/io/github/hackeraxe/App.java
+++ my_first_weather_app/src/main/java/io/github/hackeraxe/App.java
@@ -9,18 +9,35 @@ import java.net.http.HttpResponse;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+/*
+notes:
+    APIs work in the general same way. client manages the connection, the request is what we're wanting and where we're going to
+    get the information, and the response is the internet call itself, and gives us the data we want.
+
+    gson is a library that can parse jsons for us. we should know the structure of a json... but we shouldn't worry about
+    HOW it parses it. (abstraction)
+*/
 public class App{
     public static void main(String[] args){
+        //sends requests, receive responses. manages the connection
         HttpClient client = HttpClient.newHttpClient();
         String url = "https://api.open-meteo.com/v1/forecast?latitude=40.44&longitude=-79.97&current_weather=true";
         
+        //the http request uses the url to create the destination with the uri, then build finalizes the request
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
 
         try{
+
+            //the network call itself
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             String jsonString = response.body();
+
+            //gson parses the json
+
             JsonObject root = JsonParser.parseString(jsonString).getAsJsonObject();
             JsonObject current = root.getAsJsonObject("current_weather");
+
+            //Get the temp, windsped, and weathercode from the json object and split it into variables
 
             double temperature = current.get("temperature").getAsDouble();
             double windspeed = current.get("windspeed").getAsDouble();
