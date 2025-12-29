fcee35c9c32ae3e9f964568d270788c30f3627fb
Tiberius Finney <finneyaf16@gmail.com>
2025-12-29 01:56:20 -0500

    Update App.java

--- my_first_weather_app/src/main/java/io/github/hackeraxe/App.java
+++ my_first_weather_app/src/main/java/io/github/hackeraxe/App.java
@@ -16,6 +16,15 @@ notes:
 
     gson is a library that can parse jsons for us. we should know the structure of a json... but we shouldn't worry about
     HOW it parses it. (abstraction)
+
+TO DO FOR TOMORROW:
+    - split the methods via their tasks. One method to retrieve the json data, one method to parse it, and one method to 
+    display the information
+
+    - create junit tests for this application
+
+    - apply CS1530 knowledge. think about what kind of design pattern we're using here. How could we write this as a uml? 
+    what's the structure? How can we write about what we've learned in a way that can be shown to others?
 */
 public class App{
     public static void main(String[] args){
