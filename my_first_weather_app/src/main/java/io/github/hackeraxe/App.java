4489a8e0117346351d7acf02fb93f88dfa3b5dbc
Tiberius Finney <finneyaf16@gmail.com>
2025-12-30 01:14:59 -0500

    added javafx functionality

--- my_first_weather_app/src/main/java/io/github/hackeraxe/App.java
+++ my_first_weather_app/src/main/java/io/github/hackeraxe/App.java
@@ -1,36 +1,32 @@
 package io.github.hackeraxe;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
-
-
-/*
-notes:
-    APIs work in the general same way. client manages the connection, the request is what we're wanting and where we're going to
-    get the information, and the response is the internet call itself, and gives us the data we want.
-
-    gson is a library that can parse jsons for us. we should know the structure of a json... but we shouldn't worry about
-    HOW it parses it. (abstraction)
-
-TO DO FOR TOMORROW:
-    - split the methods via their tasks. One method to retrieve the json data, one method to parse it, and one method to 
-    display the information
-
-    - create junit tests for this application
-
-    - apply CS1530 knowledge. think about what kind of design pattern we're using here. How could we write this as a uml? 
-    what's the structure? How can we write about what we've learned in a way that can be shown to others?
-*/
-public class App{
+public class App extends Application{
     public static void main(String[] args){
-        Weather ourWeather = new Weather();
-        if (ourWeather != null){
-           displayWeather(ourWeather); 
-        }
-        
+        launch();
     }
 
     public static void displayWeather(Weather weather){
         System.out.println("Temperature: " + weather.getTemperature() +
         "\nWind Speed: " + weather.getWindspeed() + 
         "\nWeather Code: " + weather.getWeatherCode());
+    }
+
+    public void start(Stage primaryStage) throws Exception{
+        Weather ourWeather = new Weather();
+
+        Label tempLabel = new Label("Temperature: " + ourWeather.getTemperature());
+        Label windspeedLabel = new Label("Wind Speed: " + ourWeather.getWindspeed());
+        Label weatherCode = new Label("Weather Code: " + ourWeather.getWeatherCode());
+
+        VBox root = new VBox(10, tempLabel, windspeedLabel, weatherCode);
+        Scene sc = new Scene(root);
+        primaryStage.setTitle("The Mentally Ill Pookie Weather App");
+        primaryStage.setScene(sc);
+        primaryStage.show();
     }
 }
